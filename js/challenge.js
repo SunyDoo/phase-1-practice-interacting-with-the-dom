83af657c9df9be45e115e11b59c1e12cc5b53606
Sunil <snilavarath@gmail.com>
2021-12-16 22:07:26 -0500

    Completed Assignment

--- js/challenge.js
+++ js/challenge.js
@@ -4,7 +4,11 @@ const plus = document.getElementById('plus')
 const heart = document.getElementById('heart')
 const pause = document.getElementById('pause')
 const likes = document.querySelector('ul.likes')
-console.log(likes)
+const commentButton = document.getElementById('submit')
+commentInput = document.getElementById('comment-input')
+commentList = document.getElementById('list') 
+const likeList = {}
+
 let interval = setInterval(incrementTimer, 1000)
 function incrementTimer(){
     second.innerText = parseInt(second.innerText) + 1
@@ -12,22 +16,40 @@ function incrementTimer(){
 function decrementCounter(){
     second.innerText = parseInt(second.innerText) - 1
   }
+
 minus.addEventListener ('click', decrementCounter)
 plus.addEventListener ('click', incrementTimer)
-// heart.addEventListener ('click', )
-
+heart.addEventListener ('click', liker)
+function liker(){
+    if (second.innerText in likeList){
+        likeList[second.innerText] +=1
+    }
+    else {
+        likeList[second.innerText] = 1
+    }
+    let list = `<li>${second.innerText} was liked ${likeList[second.innerText]} times</li>`
+    likes.innerHTML += list
+}
 
 pause.addEventListener('click', pauseTime) 
 function pauseTime(){
-    if (pause.innerText = 'pause'){  
+    if (pause.innerText == 'pause'){  
     clearInterval(interval)
     pause.innerText = 'resume'
     }
-    else if (pause.innerText = 'resume'){  
+    else if (pause.innerText == 'resume'){  
         interval = setInterval(incrementTimer, 1000)
         pause.innerText = 'pause'
     }
 }
-    
-
+   
+commentButton.addEventListener('click', leaveComment)
+function leaveComment(event){
+    event.preventDefault()
+    const form = document.getElementById('comment-form')
+    const li = document.createElement("li")
+    li.innerText = commentInput.value
+    commentList.append(li)
+    form.reset()
+}
  
